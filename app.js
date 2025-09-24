58e75a55341f2a67431247aebf6a27c43c2e91b5
ritesh bhosale <riteshbhosale092007@gmail.com>
2025-09-24 19:31:15 +0530

    .

--- app.js
+++ app.js
@@ -25,6 +25,16 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .catch(err => console.log(err));
 
 app.use("/", require("./routes/authRoutes"));
+
+app.use("/", (req, res, next) => {
+    res.locals.currentUser = req.session.userId;
+    next();
+});
+
+app.use("/", (req, res, next) => {
+    res.locals.currentUser = req.session.userId;
+    next();
+});
 app.use("/events", require("./routes/eventRoutes"));
 app.use("/bookings", require("./routes/bookingRoutes"));
 
